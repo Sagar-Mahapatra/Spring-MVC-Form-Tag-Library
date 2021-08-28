b8086af5d43027ffc6565d188981e2d4dc42d2a6
DESKTOP-F4RNNA9\Sagar <sagarkmohapatra2012@gmail.com>
2021-08-28 00:14:19 -0700

    first commit

---
+++ src/main/java/in/ashokit/SpringMvcFormTagLibraryApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFormTagLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFormTagLibraryApplication.class, args);
+	}
+
+}
